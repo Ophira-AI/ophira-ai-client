aa9dfc8e44946b971529dc98eb63ded2298f68e3
egbejob680 <jobegbe39@gmail.com>
2025-05-17 18:21:22 +0100

    navigation link to account creation

---
+++ src/app/register/page.tsx
@@ -0,0 +1,16 @@
+
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function RegisterPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/register/userinfo');
+  }, [router]);
+
+  return null; 
+}
+
